7a0ccf5e8f5387db3c961f871c7e017c70c0e3f7
asrumyantsev <asrumyantsev@yahoo.com>
2020-09-08 09:58:50 +0300

    init repo

---
+++ src/main/java/pro/sofix/checkinger/CheckingerApplication.java
@@ -0,0 +1,13 @@
+package pro.sofix.checkinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckingerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckingerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sofix/checkinger/CheckingerApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sofix.checkinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckingerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
